c34287a4294d9360c2a619228d679194f54c96f5
ppmand <ppmand@gmail.com>
2019-03-13 09:02:52 +0300

    My Django blog

---
+++ ppm_grad/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ppm_grad project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ppm_grad.settings')
+
+application = get_wsgi_application()
